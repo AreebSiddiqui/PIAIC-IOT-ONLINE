5e2c4e66b0b9d26ff2fd68ee57be13d4f17a0bdd
AreebSiddiqui <areeb40@hotmail.com>
2019-10-06 19:42:05 +0500

    v1

---
+++ Chapter4/p4/src/main.rs
@@ -0,0 +1,19 @@
+// fn main() {
+//    let result = gives_onwership(); //function call
+//    println!("{}",result);
+//     let s1 = String::from("Pakistan"); // s1 comes into scope 
+//     let result1 = takes_and_gives_back(s1);  // s1 is moved into takes_and_gives_back
+//     println!("{}",result1);
+// }
+
+// fn gives_onwership () -> String {
+//     let s = String::from("Hello World"); 
+//     s
+// }
+
+
+// fn takes_and_gives_back(x: String) -> String { 
+// x // x is moved out to the calling function 
+// } 
+
+
