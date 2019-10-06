f0d0e90f370285ea711a4d44872f079027b98baf
AreebSiddiqui <areeb40@hotmail.com>
2019-10-06 19:25:10 +0500

    v1

---
+++ chapter3/loops/src/main.rs
@@ -0,0 +1,66 @@
+ // fn main (){
+//     let mut counter = 0;
+//    let val_counter =  loop  {
+//         println!("Hello World");
+//         counter = counter + 1;
+//         if (counter == 3) {
+//             break counter //expression
+//         } 
+//     };
+//  println!("{}",val_counter);
+// }
+
+
+// fn  main () {
+
+//     // let mut counter = 0;
+//     // while counter < 3 {
+//     //     println!("Hello World");
+//     //     counter = counter + 1;
+//     // }
+
+//     let mut counter_1 = 0;
+//     let lottery_number = [1,23,45,6,78,9];
+    
+//     while counter_1 < lottery_number.len() {
+//         println!("{}",lottery_number[counter_1]);
+//         counter_1 = counter_1 + 1;
+//     }
+// }
+
+
+
+
+
+
+
+fn main() {
+
+for a in (0..5).rev() {
+    println!("{}. Hello world",a);
+}
+
+let lottery_number = [1,23,45,67,89];
+    for num in lottery_number.iter(){
+        println!("{}",num);
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
