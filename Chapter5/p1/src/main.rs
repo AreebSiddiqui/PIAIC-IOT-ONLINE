9258ea6915000f250e872dcbf3529af2d3286d68
AreebSiddiqui <areeb40@hotmail.com>
2019-10-06 19:49:29 +0500

    v1

---
+++ Chapter5/p1/src/main.rs
@@ -0,0 +1,23 @@
+#[allow(unused_variables)]
+fn main () {
+
+    let book_name_1  = String::from("Book A");
+    let book_author_1 = String::from("Author A");
+    let book_price_1 = 500;
+    let book_available_1 = true;
+
+
+    let book_name_2 = String::from("Book B");
+    let book_author_2 = String::from("Author B");
+    let book_price_2 = 750;
+    let book_available_2 = false;
+
+    
+    let book_name_3 = String::from("Book C");
+    let book_author_3 = String::from("Author C");
+    let book_price_3 = 600;
+    let book_available_3 = true;
+    println!("{}, {}",book_name_1,book_author_2);
+
+}
+
