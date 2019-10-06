dab9665eb83779a3af32ea4b7e15441479c2ff19
AreebSiddiqui <areeb40@hotmail.com>
2019-10-06 19:25:18 +0500

    v1

---
+++ chapter3/numbersys/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+   let DecimalNumber = 100;
+   let HexaDecimalNumber = 0xff;
+   let OcatalNumber = 0o77;
+   let binaryNumbersys = 0b10101010;
+    let c = b'B'; 
+    println!("{}",DecimalNumber);
+    println!("{}",HexaDecimalNumber);
+    println!("{}",OcatalNumber);
+    println!("{}",binaryNumbersys);
+    println!("{}",c);
+
+}
