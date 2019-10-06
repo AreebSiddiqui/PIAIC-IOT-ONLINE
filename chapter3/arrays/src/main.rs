94fa1642da56b57f3631799e3d394d9c0f9805e7
AreebSiddiqui <areeb40@hotmail.com>
2019-10-06 19:21:06 +0500

    v1

---
+++ chapter3/arrays/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let lottery_number:[u32;5] = [1,23,45,56,78];
+//                               0   1  2  3  4
+println!("{:#?}",lottery_number);
+
+    let rupee_five = [5;10];
+    println!("{:#?}",rupee_five);
+
+
+    let days = ['M','T','W'];
+//               0   1   2    
+    println!("{:#?}",days[1]);
+
+}
