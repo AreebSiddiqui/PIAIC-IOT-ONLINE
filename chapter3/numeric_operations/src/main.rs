482271b2ebb1268e81ecfff755e0aceaff51412b
AreebSiddiqui <areeb40@hotmail.com>
2019-10-06 19:25:27 +0500

    v1

---
+++ chapter3/numeric_operations/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+let x:u32 = 9;
+let y:u32 = 5;
+
+let sum = x+y;
+let minus =x-y;
+let div = x/y;
+let mul = x*y;
+let rem = x%y;
+
+println!("{}",sum);
+println!("{}",minus);
+println!("{}",div);
+println!("{}",rem);
+println!("{}",mul);
+
+
+
+}
