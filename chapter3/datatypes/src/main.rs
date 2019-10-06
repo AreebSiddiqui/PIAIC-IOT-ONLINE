210de66b9f867d1f9a55828d674c5ad4b92605f6
AreebSiddiqui <areeb40@hotmail.com>
2019-10-06 19:24:32 +0500

    v1

---
+++ chapter3/datatypes/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    //scalar data types
+   let age= 25;
+   let percentage = 80.3;
+   let grade = 'A';
+   let passed = true;
+
+
+   let secret_number : u32 = -80;
+   println!("{}",secret_number);
+
+
+}
