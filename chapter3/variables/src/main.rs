4dc4cb18b8260e792b738a62306116d74f0ae1b3
AreebSiddiqui <areeb40@hotmail.com>
2019-10-06 19:25:44 +0500

    v1

---
+++ chapter3/variables/src/main.rs
@@ -0,0 +1,17 @@
+// fn main() {
+//     let mut temperature = 35;
+//     temperature = 30;
+//     println!("{}",temperature);
+// }
+
+
+fn main () {
+    let total:f64 = 200.0;
+    let a:f64 = 100.0;
+    let b:f64 = 60.0;
+
+
+    let marks = a + b ;
+    let percentage:f64 = (marks /total)*100.0;
+    println!("{}",percentage);
+}
